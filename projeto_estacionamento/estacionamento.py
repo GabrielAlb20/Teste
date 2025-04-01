80089febea36f1df9bc39db9d0b92ce8798d0488
GabrielAlbqr20 <gabriel.albqdev@outlook.com>
2025-04-01 14:37:22 -0300

    subindo arquivos

--- projeto_estacionamento/estacionamento.py
+++
@@ -1,8 +0,0 @@
-"""o objetivo é desenvolver um sistema simples e capaz de realizar
- algumas tarefas de gerenciamento de um estacionamento comercial.
-"""
-print("Bem vindos ao estacionamento UniCesumar Parking")
-dias_semana = ('segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabádo')
-folga = ('domingo')
-inicio_horas = 8
-fim_horas = 18
